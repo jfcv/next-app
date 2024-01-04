72ee935471f9d4fb4b937148696475e3e2476259
Juan Fernando Cuadros Valle <juan.cuadros@pm.me>
2024-01-04 01:29:23 -0500

    Add some files in the app folder which works as a router for Next.js 13. Use [Link] component to avoid re-rendering when navigating throught the page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
+import Link from "next/link";
 
+/**
+ * Client-Side Navigation
+ */
 export default function Home() {
   return (
     <main>
       <h1>Hello World</h1>
+      <Link href="/users">Users</Link>
     </main>
   )
 }
