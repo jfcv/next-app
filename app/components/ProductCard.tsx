1f119480367503ac5d1d8eb220de2aa26a807cc7
Juan Fernando Cuadros Valle <juan.cuadros@pm.me>
2024-05-02 18:44:10 -0500

    Add TailwindCSS styles. Add DaisyUI which is for Tailwind what Bootstrap is for CSS.

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -4,7 +4,13 @@ import styles from './ProductCard.module.css'
 
 const ProductCard = () => {
   return (
-    <div className={styles.card}>
+    // it uses tailwind by default
+    // the selling point of tailwind is
+    // that we can style our components from
+    // this file
+    // the downside to that is that it violates
+    // the separation of concerns principle
+    <div className='p-5 my-5 bg-sky-400 text-white text-xl hover:bg-sky-500'>
         <h2 className={styles.header}>Card #1</h2>
         <p>Lorem ipsum dolor sit amet.</p>
         
