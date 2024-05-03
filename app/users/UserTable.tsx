0f5d097a416bda28bc8ada26d1e88c807fba91ea
Juan Fernando Cuadros Valle <juan.cuadros@pm.me>
2024-05-02 19:14:06 -0500

    Extract the users table into its own component.

---
+++ app/users/UserTable.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+interface User {
+    id: number;
+    name: string;
+    email: string;
+  }
+
+const UserTable = async () => {
+  /**
+   * this means is gonna refresh data from the backend every 10 seconds
+   * this just works for fetch()
+   * but DOESN'T WORK for axios()
+   */
+    const res = await fetch('https://jsonplaceholder.typicode.com/users', {
+        next: {
+          revalidate: 10
+        }
+      })
+    const users: User[] = await res.json()
+
+  return (
+    <table className='table table-border'>
+
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {users.map(user => 
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+            </tr>
+            )}
+        </tbody>
+
+      </table>
+  )
+}
+
+export default UserTable
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,47 +1,13 @@
 import React from 'react'
+import UserTable from './UserTable'
 
-interface User {
-  id: number;
-  name: string;
-  email: string;
-}
 
 const UsersPage = async () => {
-  /**
-   * this means is gonna refresh data from the backend every 10 seconds
-   * this just works for fetch()
-   * but DOESN'T WORK for axios()
-   */
-  const res = await fetch('https://jsonplaceholder.typicode.com/users', {
-    next: {
-      revalidate: 10
-    }
-  })
-  const users: User[] = await res.json()
-
   return (
     <>
       <h1>Users</h1>
 
-      <table className='table table-border'>
-
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Email</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          {users.map(user => 
-            <tr key={user.id}>
-              <td>{user.name}</td>
-              <td>{user.email}</td>
-            </tr>
-            )}
-        </tbody>
-
-      </table>
+      <UserTable />
     </>
   )
 }
