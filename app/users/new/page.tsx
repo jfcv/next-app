c3bd3a57fff87243c60360e812fc42e2e1fcc57e
Juan Fernando Cuadros Valle <juan.cuadros@pm.me>
2024-05-19 23:28:15 -0500

    Add programmatic navigation.

--- app/users/new/page.tsx
+++ app/users/new/page.tsx
@@ -1,8 +1,20 @@
+'use client'; // <-- client component!
+import { useRouter } from 'next/navigation';
 import React from 'react'
 
+/**
+ * programmatic navigation
+ */
 const NewUserPage = () => {
+  const router = useRouter();
+
   return (
-    <div>NewUserPage</div>
+    <button 
+      className='btn btn-primary'
+      onClick={() => router.push('/users')}
+      >
+      Create
+    </button>
   )
 }
 
