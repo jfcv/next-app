4cc4dcd76de48dc80fc8f02f609cdabf4591ef6d
Juan Fernando Cuadros Valle <juan.cuadros@pm.me>
2024-05-27 15:44:34 -0500

    Add create route to /api/products.

--- app/api/products/route.tsx
+++ app/api/products/route.tsx
@@ -1,8 +1,27 @@
 import { NextRequest, NextResponse } from "next/server";
+import schema from "./schema";
 
 export function GET(request: NextRequest) {
     return NextResponse.json([
         { id: 1, name: 'Milk', price: 2.5 },
         { id: 2, name: 'Bread', price: 3.5 },
     ])
+}
+
+export async function POST(request: NextRequest) {
+    const body = await request.json();
+
+    const validation= schema.safeParse(body);
+    if(!validation.success)
+        return NextResponse.json(validation.error.errors, { status: 400 });
+
+    /**
+     * you can use the spread operator like this 
+     * { id: #, ...body }
+     * but a malicious user could send more properties
+     * than required so it's best to destructured desired
+     * props and send back just the ones required
+     */
+    const { name, price } = body;
+    return NextResponse.json({id: 3, name, price }, { status: 201 });
 }
