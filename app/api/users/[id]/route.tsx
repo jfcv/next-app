2da986e9bb70df45b0a110c22bcb65807344d0b9
Juan Fernando Cuadros Valle <juan.cuadros@pm.me>
2024-05-27 12:46:04 -0500

    Building APIs: Updating an object. Deleting an object.

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -6,9 +6,33 @@ interface Props {
     }
 }
 
-export function GET(request: NextRequestst, { params: {id} }: Props) {
+export function GET(request: NextRequest, { params: {id} }: Props) {
     if(id > 10)
         return NextResponse.json({ error: 'User not found'}, { status: 404});
 
     return NextResponse.json({ id: Number(id), name: 'Mosh'});
+}
+
+export async function PUT(request: NextRequest, { params: {id} }: Props) {
+    const body = await request.json();
+    // bad request, 400
+    if(!body.name)
+        return NextResponse.json({error: 'Name is required'}, {status: 400});
+
+    // user not found, 404
+    if(id > 10)
+        return NextResponse.json({ error: 'User not found'}, { status: 404});
+
+    // update the user
+    return NextResponse.json({ id: Number(id), name: body.name});
+}
+
+export async function DELETE(request: NextRequest, { params: {id} }: Props) {
+        // user not found, 404
+        if(id > 10)
+            return NextResponse.json({ error: 'User not found'}, { status: 404});
+
+        // delete the user from the db, in a real scenario
+
+        return NextResponse.json({});
 }
