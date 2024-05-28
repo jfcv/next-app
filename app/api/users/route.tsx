dd1a64bb8cdaf7f47e5f1ad9a5812b035bbc8c79
Juan Fernando Cuadros Valle <juan.cuadros@pm.me>
2024-05-28 15:57:11 -0500

    Fetch all users from db.

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,11 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
 import schema from "./schema";
+import prisma from "@/prisma/client";
 
-export function GET(request: NextRequest) { // to prevent caching you have to add this request to the endpoint
-    return NextResponse.json([
-        {id: 1, name: 'Mosh'},
-        {id: 2, name: 'John'},
-    ]);
+export async function GET(request: NextRequest) { // to prevent caching you have to add this request to the endpoint
+
+    const users = await prisma.user.findMany();
+
+    return NextResponse.json(users);
 }
 
 export async function POST(request: NextRequest) {
