ff48995399e98edbe66ed33d2c7f2052ae495945
Juan Fernando Cuadros Valle <juan.cuadros@pm.me>
2024-05-28 16:02:36 -0500

    Fetch one specific user from db.

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import schema from "../schema";
+import prisma from "@/prisma/client";
 
 interface Props {
     params: {
@@ -7,11 +8,17 @@ interface Props {
     }
 }
 
-export function GET(request: NextRequest, { params: {id} }: Props) {
-    if(id > 10)
+export async function GET(request: NextRequest, { params: {id} }: Props) {
+    const user = await prisma.user.findUnique({
+        where: {
+            id: Number(id)
+        }
+    })
+
+    if(!user)
         return NextResponse.json({ error: 'User not found'}, { status: 404});
 
-    return NextResponse.json({ id: Number(id), name: 'Mosh'});
+    return NextResponse.json(user);
 }
 
 export async function PUT(request: NextRequest, { params: {id} }: Props) {
