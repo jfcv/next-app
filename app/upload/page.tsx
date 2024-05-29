c7837e6428292abc68c9ff0ec969cdabc470ea83
Juan Fernando Cuadros Valle <juan.cuadros@pm.me>
2024-05-29 13:02:08 -0500

    Add support to show uploaded images at /upload endpoint.

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,17 +1,37 @@
 'use client'
-import React from 'react'
-import { CldUploadWidget } from 'next-cloudinary'
+import React, { useState } from 'react'
+import { CldUploadWidget, CldImage } from 'next-cloudinary'
+
+interface CloudinaryResult {
+  public_id: string;
+}
 
 const UploadPage = () => {
+  const [publicId, setPublicId] = useState('');
+
   return (
-    <CldUploadWidget uploadPreset='h3bx2uhx'>
-        {({ open }) => 
-            <button 
-                className='btn btn-primary' 
-                onClick={() => open()}>
-                    Upload
-            </button>}
-    </CldUploadWidget>
+    <>
+      {publicId && 
+        <CldImage src={publicId} width={270} height={180} alt='A random image' />
+      }
+      <CldUploadWidget 
+        uploadPreset='h3bx2uhx'
+        onUpload={(result, widget) => {
+          if(result.event !== 'success') return;
+          const info = result.info as CloudinaryResult;
+          setPublicId(info.public_id);
+        }}
+      >
+          {({ open }) => 
+              <button 
+                  className='btn btn-primary' 
+                  onClick={() => open()}
+              >
+                      Upload
+              </button>
+          }
+      </CldUploadWidget>
+    </>
   )
 }
 
