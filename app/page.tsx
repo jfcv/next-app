c21b9785a1a1fd7b3e4723f26f5362781ed99795
Juan Fernando Cuadros Valle <juan.cuadros@pm.me>
2024-06-06 18:57:06 -0500

    Add session access on the server.

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,19 @@
 import Link from "next/link";
 import ProductCard from "./components/ProductCard";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/route";
 
 /**
  * Client-Side Navigation
  */
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+
   return (
     <main>
-      <h1 style={{fontWeight: 'bold'}}>Hello World</h1>
+      <h1 style={{fontWeight: 'bold'}}>
+        Hello { session && <span className='text-violet-600'>{session.user!.name}</span> }
+      </h1>
 
       <Link href="/users">Users</Link>
 
