f3cd25574c2ee6eaa1496ae13d7a83b51647478e
Juan Fernando Cuadros Valle <juan.cuadros@pm.me>
2024-06-06 16:44:13 -0500

    Add Auth using github and credentials providers.

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -3,11 +3,12 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <nav className='flex bg-slate-200 p-5'>
-        <Link href='/' className='mr-5'>Next.js</Link>
-        <Link href='/users' className='mr-5'>Users</Link>
-        <Link href='/products' className='mr-5'>Products</Link>
-        <Link href='/admin' className='mr-5'>Admin</Link>
+    <nav className='flex bg-slate-200 p-5 space-x-10'>
+        <Link href='/'>Next.js</Link>
+        <Link href='/users'>Users</Link>
+        <Link href='/products'>Products</Link>
+        <Link href='/admin'>Admin</Link>
+        <Link href='/api/auth/signin'>Login</Link>
     </nav>
   )
 }
