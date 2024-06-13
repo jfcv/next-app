7f710c8a63a631b0d72cf27d1c20a512109ccbae
Juan Fernando Cuadros Valle <juan.cuadros@pm.me>
2024-06-13 18:18:58 -0500

    Add lazy load example w/ [ HeavyComponent.tsx ].

---
+++ app/components/HeavyComponent.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HeavyComponent = () => {
+  return (
+    <div className='mt-5'>My Heavy Component</div>
+  )
+}
+
+export default HeavyComponent
--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,38 @@
+'use client'
+
 import Link from "next/link";
 import ProductCard from "./components/ProductCard";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
+// import HeavyComponent from "./components/HeavyComponent"; // -> page.js bundle size 112kb
+import { useState } from "react";
+import dynamic from "next/dynamic";
+// Lazy loading
+const HeavyComponent = dynamic(
+  () => import('./components/HeavyComponent') // -> page.js bundle size 116kb
+);
 
 /**
  * Client-Side Navigation
  */
-export default async function Home() {
-  const session = await getServerSession(authOptions);
+export default function Home() {
+  //const session = await getServerSession(authOptions);
+  const [isVisible, setVisible] = useState(false);
 
   return (
     <main>
       <h1 style={{fontWeight: 'bold'}}>
-        Hello { session && <span className='text-violet-600'>{session.user!.name}</span> }
+        Hello
+        {/* Hello { session && <span className='text-violet-600'>{session.user!.name}</span> } */}
       </h1>
 
       <Link href="/users">Users</Link>
 
       <ProductCard />
+
+      <button className="btn mt-5" onClick={() => setVisible(true)}>Show</button>
+
+      {isVisible && <HeavyComponent />}
     </main>
   )
 }
